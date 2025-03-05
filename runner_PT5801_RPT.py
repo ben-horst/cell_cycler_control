8e930e1b24838672fde452bb7abb4447e49ab567
ben-horst <139262920+ben-horst@users.noreply.github.com>
2025-03-05 11:01:46 -0800

    Tested successfully new runner for PT5801

--- runner_PT5801_RPT.py
+++ runner_PT5801_RPT.py
@@ -1,11 +1,9 @@
 import PT5801_config as CONFIG
 from test_runner import TestRunner
 
-#profile = "C:/Users/cell.test/Documents/Current Test Profiles/RPTs/P45_RPT_V1.2.xml"
-profile = "C:/Users/cell.test/Documents/Current Test Profiles/Specific Tests/rest_with_10Hz_log.xml"
+profile = "C:/Users/cell.test/Documents/Current Test Profiles/RPTs/P45_RPT_V1.2.xml"
 savepath = "C:/Users/cell.test/Documents/Test Data/PT-5801/RPTs"
-#temps = [20, 35, 50]
-temps = [25, 30]
+temps = [20, 35, 50]
 test_title = 'PT5801_RPTs'
 
 
@@ -31,10 +29,16 @@ for specimen in specimens:
     filenames.append(f'{specimen}_RPT_after_{cycle}_cycles')
 
 test_runner = TestRunner(channels, test_title)
+barcodes = test_runner.barcodes
 
 for temp in temps:
     test_runner.bring_all_cells_to_temp_and_block_until_complete(temp=temp, timeout_mins=60)
     test_runner.start_tests(channels, profile, savepath, filenames)
     test_runner.wait_for_all_channels_to_finish_and_block_until_complete(timeout_mins=60*48)
 
-test_runner.send_email(f'{test_title} Test Complete', f'All tests completed successfully.\n\nBanks used: {active_banks}\n\nSpecimens tested: {specimens}')
+test_runner.send_email(f'{test_title} Test Complete',
+                       f'''All tests completed successfully for multitemp RPT for PT-5801.
+                       \n\nRPT temperatures: {temps} degC
+                       \n\nBanks tested: {active_banks}
+                       \n\nSpecimens tested: {specimens}
+                       \n\nCells Tested: {barcodes}''')
--- test.py
+++
@@ -1,5 +0,0 @@
-from temperature_control import TemperatureController
-
-chiller = TemperatureController('COM8')
-
-print(chiller.read_temperature())
