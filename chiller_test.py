fb5e65bee9b7f76bc03ea45052db8ef15b1f5b63
ben-horst <139262920+ben-horst@users.noreply.github.com>
2025-05-15 16:20:56 -0700

    Added multiple retries on closing port. Changed baudrate to 9600

--- chiller_test.py
+++ chiller_test.py
@@ -6,11 +6,8 @@ temp = 24
 
 chiller = TemperatureController(port)
 
-print('set #1')
 chiller.set_temperature(temp)
-time.sleep(60)
-print('set #2')
-chiller.set_temperature(temp)
+print(chiller.read_temperature())
 
 #COM3 - 5901
 #COM4 - 5801
--- core/temperature_control.py
+++ core/temperature_control.py
@@ -3,18 +3,23 @@ import sys
 import serial
 import time
 class TemperatureController:
-    def __init__(self, port, baudrate=115200, timeout=1):
+    def __init__(self, port, baudrate=9600, timeout=1):
         self.port = port
         self.baudrate = baudrate
         self.timeout = timeout
         self.open_comport()
-        #self.close_comport()
+        self.close_comport()
     def __str__(self):
         return f'Chiller controller object on comport {self.serial_port}'
     def open_comport(self):
         self.serial_port = serial.Serial(self.port, baudrate=self.baudrate, bytesize=8, parity='N', stopbits=1, timeout=self.timeout)
-    def close_comport(self):
-        self.serial_port.close()
+    def close_comport(self, retries = 5):
+        for attempt in range(1, retries+1):
+            self.serial_port.close()
+            if not self.serial_port.is_open:
+                break
+            else:
+                attempt += 1
     def set_temperature(self, temperature):
         command = f"SS{temperature}"
         self._send_command(command)
